9437cc1c30c3aab254e403b241c83263ccb8e888
Jeffrey Grizzle <jeffgrizzle77@gmail.com>
2020-05-26 17:38:58 -0400

    Added FatCow icons

---
+++ src/DotNetPow.Web.Icons/FatCow/FatCowIconExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetPow.Web.Icons.FatCow
+{
+    public static class FatCowIconExtensions
+    {
+        public static string ToFatCowIconCssClass(FatCowIconBlackNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconBlackCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconBlueNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconBlueCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconBrownNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconBrownCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconGreenNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconGreenCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconGreyNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconGreyCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconMixNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconMixCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconOrangeNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconOrangeCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconRedNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconRedCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconVioletNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconVioletCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconWhiteNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconWhiteCssClassNames[name];
+        }
+
+        public static string ToFatCowIconCssClass(FatCowIconYellowNames name)
+        {
+            return StaticFatCowIconDefinitions.FatCowIconYellowCssClassNames[name];
+        }
+    }
+}
